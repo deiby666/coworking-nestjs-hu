c77f6795f552b4f5ae0bf90163a090ae0b2ec68c
deiby666 <cardonadeiby88@gmail.com>
2024-06-21 14:48:14 -0500

    swagger requerido y funcional

--- src/espacios/espacios.controller.ts
+++ src/espacios/espacios.controller.ts
@@ -5,12 +5,14 @@ import { UpdateEspacioDto } from './dto/update-espacio.dto';
 import { Espacio } from './entities/espacio.entity';
 import { EspacioDTO } from './dto/espacioDto';
 import { log } from 'console';
-
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+@ApiTags('espacios')
 @Controller('espacios')
 export class EspaciosController {
   constructor(private readonly espaciosService: EspaciosService) {}
 
   @HttpCode(HttpStatus.OK)
+  @ApiOperation({ summary: 'Obtiene todos los espacios que esten disponibles' })
   @Get('available/sesion/:idSesion')
   async obtenerEspaciosDisponibles(@Param('idSesion') idSesion: number): Promise<EspacioDTO[]> {
     
@@ -19,6 +21,7 @@ export class EspaciosController {
   }
 
   @HttpCode(HttpStatus.OK)
+  @ApiOperation({ summary: 'Obtiene todos los espacios que estan ocupados' })
   @Get('ocuppied/sesion/:idSesion')
   async obtenerEspaciosOcupados(@Param('idSesion') idSesion: number): Promise<EspacioDTO[]> {
     
